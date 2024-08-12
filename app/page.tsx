a532c0fd358836e571c701c042cfcc0793f2fc45
saad <saadshamim@Saads-MacBook-Pro.local>
2024-08-12 12:17:49 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,63 @@
+'use client';
+import { usePrivy } from '@privy-io/react-auth';
+import WebApp from '@twa-dev/sdk';
+import { read } from 'fs';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+interface UserData {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code: string;
+  is_premium?: boolean;
+}
+
+export default function Home() {
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const {
+    ready,
+    authenticated,
+    user,
+    logout,
+    linkEmail,
+    linkWallet,
+    unlinkEmail,
+    linkPhone,
+    unlinkPhone,
+    unlinkWallet,
+    linkGoogle,
+    unlinkGoogle,
+    linkTwitter,
+    unlinkTwitter,
+    linkDiscord,
+    unlinkDiscord,
+  } = usePrivy();
+
+  console.log(ready);
+
+  useEffect(() => {
+    if (WebApp.initDataUnsafe.user) {
+      console.log(WebApp.initDataUnsafe.user);
+      setUserData(WebApp.initDataUnsafe.user as UserData);
+    }
+  }, []);
+
+  return (
+    <main>
+      test
+      <ul>
+        <li>Ready: {ready ? 'Yes' : 'No'}</li>
+        <li>Authenticated: {authenticated ? 'Yes' : 'No'}</li>
+        <li>User: {user ? 'Yes' : 'No'}</li>
+        <li>ID: {userData?.id}</li>
+        <li>First Name: {userData?.first_name}</li>
+        <li>Last Name: {userData?.last_name || 'N/A'}</li>
+        <li>Username: {userData?.username || 'N/A'}</li>
+        <li>Language Code: {userData?.language_code}</li>
+        <li>Is Premium: {userData?.is_premium ? 'Yes' : 'No'}</li>
+      </ul>
+    </main>
+  );
+}
