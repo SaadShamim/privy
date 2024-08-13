baafdcbd7a3afb078808695c2f366b893be429cf
saad <saadshamim@Saads-MacBook-Pro.local>
2024-08-13 14:41:11 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -69,8 +69,22 @@ interface UserData {
 export default function Home() {
   const [userData, setUserData] = useState<UserData | null>(null);
   const { ready, authenticated, user } = usePrivy();
-  const { logout, linkEmail, linkWallet, unlinkEmail, linkPhone, unlinkPhone, unlinkWallet, linkGoogle, unlinkGoogle, linkTwitter, unlinkTwitter, linkDiscord, unlinkDiscord } =
-    usePrivy();
+  const {
+    logout,
+    linkEmail,
+    linkWallet,
+    unlinkEmail,
+    linkPhone,
+    unlinkPhone,
+    unlinkWallet,
+    linkGoogle,
+    unlinkGoogle,
+    linkTwitter,
+    unlinkTwitter,
+    linkDiscord,
+    unlinkDiscord,
+    linkFarcaster,
+  } = usePrivy();
 
   console.log(ready);
 
@@ -115,30 +129,23 @@ export default function Home() {
       )}
       <br />
       <br />
-      {googleSubject ? (
-        <button
-          onClick={() => {
-            unlinkGoogle(googleSubject);
-          }}
-          className='text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500'
-          disabled={!canRemoveAccount}
-        >
-          Unlink Google
-        </button>
-      ) : (
-        <button
-          onClick={() => {
-            linkGoogle();
-          }}
-          className='text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white'
-        >
-          Link Google
-        </button>
-      )}
+      <button
+        onClick={() => {
+          linkGoogle();
+        }}
+        className='text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white'
+      >
+        Link Google
+      </button>
       <br />
       <br />
       <button onClick={linkWallet} className='text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white border-none'>
         Connect wallet
+      </button>
+      <br />
+      <br />
+      <button onClick={linkFarcaster} className='text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white border-none'>
+        Connect Farcaster
       </button>
     </main>
   );
