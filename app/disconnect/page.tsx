60e5f4ac85ce89957968781c85b818d3ac6dc702
saad <saadshamim@saads-mbp.mynetworksettings.com>
2024-08-23 15:26:04 -0400

    update

--- app/disconnect/page.tsx
+++ app/disconnect/page.tsx
@@ -2,46 +2,56 @@
 
 import { usePrivy } from '@privy-io/react-auth';
 import { useEffect, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+// import { useRouter, useSearchParams } from 'next/navigation';
 
-export default function Disconnect() {
-  const router = useRouter();
+import { useSearchParams } from 'next/navigation';
+
+const DisconnectClient = () => {
   const searchParams = useSearchParams();
+  const type = searchParams.get('type');
+  const address = searchParams.get('address');
 
   const [userData, setUserData] = useState(null);
   const { unlinkEmail, unlinkWallet, unlinkPhone, unlinkGoogle, unlinkTwitter, unlinkDiscord } = usePrivy();
 
-  useEffect(() => {
-    const type = searchParams.get('type');
-    const address = searchParams.get('address');
-    const usernameOrId = searchParams.get('usernameOrId');
+  //   useEffect(() => {
+  //     const type = searchParams.get('type');
+  //     const address = searchParams.get('address');
+  //     const usernameOrId = searchParams.get('usernameOrId');
 
-    if (type) {
-      if (type === 'wallet' && address) {
-        unlinkWallet(address);
-      } else if (usernameOrId) {
-        switch (type) {
-          case 'email':
-            unlinkEmail(usernameOrId);
-            break;
-          case 'phone':
-            unlinkPhone(usernameOrId);
-            break;
-          case 'google':
-            unlinkGoogle(usernameOrId);
-            break;
-          case 'twitter':
-            unlinkTwitter(usernameOrId);
-            break;
-          case 'discord':
-            unlinkDiscord(usernameOrId);
-            break;
-          default:
-            console.log(`No action for type: ${type}`);
-        }
-      }
-    }
-  }, [searchParams, unlinkWallet, unlinkEmail, unlinkPhone, unlinkGoogle, unlinkTwitter, unlinkDiscord]);
+  //     if (type) {
+  //       if (type === 'wallet' && address) {
+  //         unlinkWallet(address);
+  //       } else if (usernameOrId) {
+  //         switch (type) {
+  //           case 'email':
+  //             unlinkEmail(usernameOrId);
+  //             break;
+  //           case 'phone':
+  //             unlinkPhone(usernameOrId);
+  //             break;
+  //           case 'google':
+  //             unlinkGoogle(usernameOrId);
+  //             break;
+  //           case 'twitter':
+  //             unlinkTwitter(usernameOrId);
+  //             break;
+  //           case 'discord':
+  //             unlinkDiscord(usernameOrId);
+  //             break;
+  //           default:
+  //             console.log(`No action for type: ${type}`);
+  //         }
+  //       }
+  //     }
+  //   }, [searchParams, unlinkWallet, unlinkEmail, unlinkPhone, unlinkGoogle, unlinkTwitter, unlinkDiscord]);
 
-  return <main>Disconnecting...</main>;
-}
+  return (
+    <div>
+      <p>Type: {type}</p>
+      <p>Address: {address}</p>
+    </div>
+  );
+};
+
+export default DisconnectClient;
