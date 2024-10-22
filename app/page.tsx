b3fd1690f4a515b6d304cd2bdf449a4e3f5474e9
saad <saadshamim@gmail.com>
2024-10-22 10:29:05 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -131,7 +131,8 @@ export default function Home() {
 
   return (
     <main>
-      Creating Account...
+      ths is the dev build Creating Account...
+      {process.env.NEXT_PUBLIC_SERVER_URL}
       <CookieDisplay />
     </main>
   );
