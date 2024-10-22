edf6a6e36d7815b1239bf8e476227f5806e8d895
saad <saadshamim@gmail.com>
2024-10-22 15:26:17 -0400

    privy id fallback

--- app/config.js
+++ app/config.js
@@ -1,2 +1,2 @@
 export const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || '';
-export const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID || '';
+export const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID || 'clzila17c07u5h432nvndhqeq';
